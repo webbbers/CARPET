15aad9b7ea8a7536af20691012d2c4afcc6c4438
unsel <tunsel1@binghamton.edu>
2020-11-06 22:16:45 +0300

    added simple temporary navigation

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+
+
 
 import './header.styles.scss';
 
 const Header = () => (
-    <div class="u-center-text u-margin-bottom-big">
-        <h2 class="heading-secondary">
-            Examination made easier
-        </h2>
+    <div>
+        <div className="tempnavigation">
+            <span><Link to="/" className="btn-text">HomePage &rarr;</Link></span>
+            <span> <Link to="/signin" className="btn-text">SignIn &rarr;</Link></span>
+        </div>
+        <div class="u-center-text u-margin-bottom-big">
+            <h2 class="heading-secondary">
+                Examination made easier
+            </h2>
+        </div>
     </div>
 )
 
