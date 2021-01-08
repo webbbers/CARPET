bb3e5dfc710c65673d1951f011d7ee16db232bb2
unsel <tunsel1@binghamton.edu>
2021-01-09 01:00:57 +0300

    added score and singleQuestion at a time to exam component and some styling changes

--- src/components/exam/exam.component.jsx
+++ src/components/exam/exam.component.jsx
@@ -1,35 +1,61 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Question from '../question/question.component';
-
-
-
-
+import './exam.styles.scss'
 
 const Exam = () => {
 
+    const [currentQuestion,setCurrentQuestion] = useState(0)
+    const [score,setScore] = useState(0)
+    const [showScore,setShowScore] = useState(false)
+    const maxScore=25
     const questions = [
         {
             type:"MultipleChoice",
+            points:15,
             imageURL: "https://avatars2.githubusercontent.com/u/54589572?s=460&u=b663e9b613864114600790beb68d7d1d45b9cbde&v=4",
-            question:"What is my name",
-            options:["Talha","Tayfur","Ahmet","Mehmet"],
-            correctAnswer:"Talha"
+            questionText:"What is my name ?",
+            answerOptions : [
+                { answerText: 'Talha', isCorrect: true},
+                { answerText: 'Tayfur', isCorrect: false},
+                { answerText: 'Sarp', isCorrect: false},
+                { answerText: 'Kuzey', isCorrect: false},
+            ]
+            
         },
         {
             type:"MultipleChoice",
+            points:10,
             // imageURL: "https://avatars2.githubusercontent.com/u/54589572?s=460&u=b663e9b613864114600790beb68d7d1d45b9cbde&v=4",
-            question:"What is my name",
-            options:["Talha","Tayfur","Ahmet","Mehmet"],
-            correctAnswer:"Talhaa"
+            questionText:"How old am I ?",
+            answerOptions : [
+                { answerText: '18', isCorrect: false},
+                { answerText: '28', isCorrect: false},
+                { answerText: '22', isCorrect: true},
+                { answerText: '29', isCorrect: false},
+            ]
+            
         }
     ]
 
-
+    const approve = (correct,point) => {
+        setCurrentQuestion(currentQuestion+1)
+        if (correct) {
+            setScore(score+point)
+        }
+    }
     return (
         <div className='exampage'>
-            {questions.map( (question,index) => (
+            {/* {questions.map( (question,index) => (
                 <Question question={question} index={index+1}/>
-            ))}
+            ))} */}
+
+            {questions.length > currentQuestion ? 
+                <Question question={questions[currentQuestion]} questionNumber={currentQuestion+1} approve = {(correct,point)=>approve(correct,point)} /> 
+                : 
+                <div className="examResults">
+                    <h1>END OF EXAM</h1>
+                    <p1> You have scored {score} out of {maxScore}</p1>
+                </div>}
         </div>
     )
     
--- src/components/question/question.component.jsx
+++ src/components/question/question.component.jsx
@@ -1,42 +1,51 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 import './question.styles.scss'
 
 const Question = props => {
 
-    const { question,index } = props;
-
-    // const question =
+    const { question,questionNumber,approve } = props;
+    const [correct,setCorrect] = useState(0)  // 1 for false 2 for true
+    const optionClickedHandler = status => {
+        if (status) {
+            setCorrect(true)
+        } else {
+            setCorrect(false)
+        }
+    }
+    // example Question : 
+        // type:"MultipleChoice",
+        // imageURL: "https://avatars2.githubusercontent.com/u/54589572?s=460&u=b663e9b613864114600790beb68d7d1d45b9cbde&v=4",
+        // questionText:"What is my name ?",
+        // answerOptions : [
+        //     { answerText: 'Talha', isCorrect: true},
+        //     { answerText: 'Tayfur', isCorrect: false},
+        //     { answerText: 'Sarp', isCorrect: false},
+        //     { answerText: 'Kuzey', isCorrect: false},
+        // ]
+        
     
-    // {
-    //     type:"MultipleChoice",
-	// 	imageURL: "https://avatars2.githubusercontent.com/u/54589572?s=460&u=b663e9b613864114600790beb68d7d1d45b9cbde&v=4",
-    //     question:"What is my name",
-    //     options:["Talha","Tayfur","Ahmet","Mehmet"],
-    //     correctAnswer:"Talha"
-    // }
 
     return (
         <div className="question">
             <div className="questionInfo">
-                <div className="pointInfo"> 5 Points </div>
-                <div className="numberInfo">Question {index}</div>
+                <div className="pointInfo"> {question.points} Points </div>
+                <div className="numberInfo">Question {questionNumber}</div>
             </div>
 			{question.imageURL ? <div><img id="qphoto" alt="questImage" className="img" src={question.imageURL}/></div> : null}
-            <div className="quest"> {question.question} &nbsp;?</div>
-			<form>
-                {question.options.map( option => (
-                    
-                    <label className="optionContainer">
-                        <span className="option"></span>{option}
-                        <input type="checkbox" />
-                        <span className="checkmark"></span>
-                    </label>
-                    
-                    
-                    ))
-                }
-			</form>
+            <div className="questionText"> {question.questionText} </div>
+            <div className="answers">
+                <form>
+                    {question.answerOptions.map( answerOption => (
+                        <div>
+                            <input type="radio" name="radAnswer" onClick={()=>optionClickedHandler(answerOption.isCorrect)}/>
+                            <label className="answerOption">{answerOption.answerText}</label>
+                        </div>
+                    ))}
+                </form>
+            </div>
+	
+           <div className="approve"> <button class="btn btn--green" onClick={()=>approve(correct,question.points)}> APPROVE </button></div>
         </div>
     )
 }
